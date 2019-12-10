d18242bd487195912f2d6f6227fe5fe0d97fc02f
Manuel Nila <manila@users.noreply.github.com>
2019-12-10 15:46:03 -0800

    updated itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mnila <mnila@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 15:50:42 by mnila             #+#    #+#             */
-/*   Updated: 2019/12/10 15:19:52 by mnila            ###   ########.fr       */
+/*   Updated: 2019/12/10 15:40:49 by mnila            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,18 @@ char		*ft_itoa(int n)
 	str = ft_strnew(len);
 	if (str == NULL)
 		return (NULL);
+	if (nl == 0)
+		str[0] = '0';
 	str[len] = '\0';
 	if (nl < 0)
 	{
 		str[0] = '-';
 		nl = -nl;
 	}
-	while (nl / 10)
+	while (len >= 0 && nl)
 	{
 		str[len-- - 1] = (char)((nl % 10) + '0');
 		nl /= 10;
 	}
-	str[len - 1] = (char)((nl % 10) + '0');
 	return (str);
 }
